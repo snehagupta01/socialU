6d7d01fac61755e5ec079b381ddda7afe3708a06
snehagupta01 <sneha2001gupta@gmail.com>
2021-02-12 19:16:46 +0530

    handling sign in

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -4,6 +4,7 @@
 const User = require("../models/user");
 
 module.exports.profile=function(req,res){
+    console.log(req.cookies);
     res.send("<h1>Profile Page : SG</h1>")
 }
 
@@ -56,4 +57,36 @@ module.exports.create=function(req,res){
 //sign in and create session for users
 module.exports.createSession=function(req,res){
     ///later
+    console.log(req.body);
+    //authentication
+    //find the user
+    User.findOne({email:req.body.email},function(err,user){
+        if(err)
+        {
+            console.log("error in finding user in signing in");
+            return;
+        }
+        //handle user found
+        if(user){
+            //handle password which doesn't match
+            if(user.password!=req.body.password)
+            {
+                return res.redirect('back');
+            }
+            //handle session creation
+            res.cookie('user_id',user._id);
+            return res.redirect('/users/profile');
+        }
+        else{
+            //handle user not found
+            return res.redirect('back');
+        }
+    });
+    
+
+    
+
+    
+
+    
 }
