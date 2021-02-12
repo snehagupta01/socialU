d203d8dc608f000372617b930e9ce08e95436007
snehagupta01 <sneha2001gupta@gmail.com>
2021-02-12 13:41:42 +0530

    +views

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,3 +1,6 @@
 module.exports.home=function(req,res){
-    res.end("<h1> express is up for socialu");
+    //res.end("<h1> express is up for socialu");
+    return res.render('home',{
+        title:"SG !!!" //pass the context
+    });
 }
---
+++ models/user.js
@@ -0,0 +1,23 @@
+const mongoose=require('mongoose');
+
+const userSchema=mongoose.newSchema({
+    email:{
+        type:String,
+        required:true,
+        unique:true
+    },
+    password:{
+        type:String,
+        required:true
+    },
+    name:{
+        type:String,
+        required:true
+    }
+},{
+    timeStamps:true
+});
+
+const User=mongoose.model('User',userSchema);
+
+module.exports=User;
