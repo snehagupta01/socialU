349c964508c010a4b7809550f562b9566842a4a4
snehagupta01 <sneha2001gupta@gmail.com>
2021-02-14 16:26:41 +0530

    deleting posts

--- controllers/posts_controller.js
+++ controllers/posts_controller.js
@@ -1,4 +1,5 @@
 const Post=require('../models/post');
+const Comment=require('../models/comment');
 
 module.exports.create=function(req,res){
     Post.create({
@@ -11,4 +12,24 @@ module.exports.create=function(req,res){
         }
         return res.redirect('back');
     })
-};
+}
+
+module.exports.destroy=function(req,res){
+    Post.findById(req.params.id,function(err,post){
+        //.id -> converts objectId into string
+        if(post.user==req.user.id)//post ka ppa==jo login h(post belongs to user esa sa)
+        {
+            post.remove();
+            //delete comments bhi Comment model s no
+
+            Comment.deleteMany({post:req.params.id},function(err){
+                return res.redirect('back');
+            });
+
+        }
+        else
+        {
+            return res.redirect('back');
+        }
+    });
+}
--- routes/posts.js
+++ routes/posts.js
@@ -6,5 +6,7 @@ const router=express.Router();
 const postsController=require('../controllers/posts_controller');
 
 router.post('/create',passport.checkAuthentication,postsController.create);
+router.get('/destroy/:id',passport.checkAuthentication,postsController.destroy);
+
 
 module.exports=router;
