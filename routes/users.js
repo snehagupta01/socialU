2c3d46ea0c373b98ef0f9f92e535412f172a9f43
snehagupta01 <sneha2001gupta@gmail.com>
2021-02-16 02:03:26 +0530

    updating user's profile

--- routes/users.js
+++ routes/users.js
@@ -7,6 +7,8 @@ const usersController=require('../controllers/users_controller');
 console.log("profle loaded");
 
 router.get('/profile/:id',passport.checkAuthentication,usersController.profile);
+router.post('/update/:id',passport.checkAuthentication,usersController.update);
+
 router.get('/sign-up',usersController.signUp);
 router.get('/sign-in',usersController.signIn);
 router.post('/create',usersController.create);
